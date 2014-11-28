74866bba66e3df397d19c40c185bb2ed73fb0fe2
unknown <ethanCrobison@gmail.com>
2014-11-28 16:39:11 -0600

    Change File Names

--- CatchThatFood.py
+++ CatchThatFood.py
@@ -1,9 +1,49 @@
+##
+## CatchThatFood!
+##
+## Created by Ethan Robison & Michael Wang
+##
+## EECS 332 Digital Image Analysis Fall 2014
+## Professor Ying Wu
+##
+
+
 import numpy as np
 import cv2
+##import random, time
 
-img = cv2.imread('Comfy_Doge.jpg', 0)
-cv2.imwrite('ComfyDogeGray.jpg', img)
-##cv2.namedWindow('Image', cv2.WINDOW_NORMAL)
-##cv2.imshow('Image', img)
+##  pass 0 or -1 to use default cam
+cap = cv2.VideoCapture(0)
+ret, frame = cap.read()
+
+##  create window ahead of time
+cv2.namedWindow('Gray', cv2.WINDOW_NORMAL)
+
+##  check that frame is valid
+while frame is None:
+    ret, frame = cap.read()
+
+
+##--------------- Main Loop ---------------------------------------------
+while True:
+##  read frame  
+    ret, frame = cap.read()
+
+##  operate on frame
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+##  display frame
+    cv2.imshow('Gray', gray)
+
+
+
+##  check for quitting
+    if cv2.waitKey(1) & 0xFF == ord('m'):
+        break
+
+##-----------------------------------------------------------------------
+
 cv2.waitKey(0)
+##  mission critical
+cap.release()
 cv2.destroyAllWindows()
